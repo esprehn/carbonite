ab4c56a6bb03908a675357a5c7c71dc004d5acf6
Elliott Sprehn <esprehn@chromium.org>
2015-03-25 16:07:57 -0700

    Add DomParser and tests.
    This mostly follows the Sky parser rules, not html.

--- dom/domparser.js
+++ dom/domparser.js
@@ -3,6 +3,19 @@
 // found in the LICENSE file.
 "use strict";
 
+(function(exports) {
+
+const GREATER_THAN = 62;
+const LESS_THAN = 60;
+const EQUALS = 61;
+const SINGLE_QUOTE = 39;
+const DOUBLE_QUOTE = 34;
+const SPACE = 32;
+const NEW_LINE = 10;
+const CARRIAGE_RETURN = 13;
+const TAB = 9;
+const SLASH = 47;
+
 class DomParser {
     constructor(string) {
         this.string = string;
@@ -13,31 +26,153 @@ class DomParser {
     }
 
     parse() {
-        while (this.hasMoreChars())
+        while (this.hasMoreChars()) {
+            // console.log({state: this.state}, this.string.substring(this.offset));
             this[this.state]();
+        }
         return this.stack[0];
     }
 
     parseText() {
-        var startOffset = this.offset;
-        var charCode = 0;
-        while (this.hasMoreChars()) {
-            charCode = this.currentCharCode();
-            this.offset++;
-            if (charCode == 60)
-                break;
-        }
-        if (startOffset < this.offset) {
-            var value = this.string.substring(this.offset, startOffset);
+        var value = this.consumeStringUntil(LESS_THAN);
+        if (value) {
             var text = new Text(value);
-            this.stack[0].append(text);
+            this.currentNode().append(text);
         }
-        if (charCode == 60)
-            this.state = "element";
+        if (this.currentCharCode() == LESS_THAN) {
+            this.offset++;
+            this.state = "parseTagName";
+        }
+        // console.log("parseText", value);
     }
 
-    parseElement() {
-        console.log(this.string.substring(this.offset));
+    parseTagName() {
+        var endTag = false;
+        if (this.currentCharCode() == SLASH) {
+            this.offset++;
+            endTag = true;
+        }
+        var tagName = this.consumeTagIdent();
+        if (tagName && endTag) {
+            var node = this.currentNode();
+            // Implicitly close all mis-nested tags.
+            while (node.tagName != tagName && this.stack.length > 1) {
+                this.stack.pop();
+            }
+            if (node.tagName == tagName)
+                 this.stack.pop();
+            this.skipWhitespace();
+            if (this.currentCharCode() == GREATER_THAN)
+                this.offset++;
+            this.state = "parseText";
+        } else if (tagName && !endTag) {
+            var element = new Element(tagName);
+            this.currentNode().append(element);
+            if (this.currentCharCode() == SLASH) {
+                this.offset++;
+                if (this.currentCharCode() == GREATER_THAN) {
+                    this.offset++;
+                    endTag = true;
+                }
+            }
+            if (!endTag) {
+                this.stack.push(element);
+                this.state = "parseAttribute";
+            } else {
+                this.state = "parseText";
+            }
+        }
+
+        // console.log("parseTagName", tagName, {endTag: endTag});
+    }
+
+    parseAttribute() {
+        this.skipWhitespace();
+        var name = this.consumeAttriuteName();
+        var value = "";
+        this.skipWhitespace();
+        var charCode = this.currentCharCode();
+        if (charCode == EQUALS) {
+            this.offset++;
+            this.skipWhitespace();
+            charCode = this.currentCharCode();
+            if (charCode == SINGLE_QUOTE || charCode == DOUBLE_QUOTE) {
+                this.offset++;
+                value = this.consumeStringUntil(charCode);
+                this.offset++;
+            } else {
+                value = this.consumeTagIdent();
+            }
+        }
+        if (name)
+            this.currentNode().setAttribute(name, value || "");
+        this.skipWhitespace();
+        charCode = this.currentCharCode();
+        var endTag = false;
+        if (charCode == SLASH) {
+            this.offset++;
+            endTag = true;
+        }
+        charCode = this.currentCharCode();
+        if (charCode == GREATER_THAN) {
+            if (endTag)
+                this.stack.pop();
+            this.offset++;
+            this.state = "parseText";
+        }
+        // console.log("parseAttribute", name, value);
+    }
+
+    consumeAttriuteName() {
+        var startOffset = this.offset;
+        while (this.hasMoreChars() && !this.isWhitespaceChar()) {
+            var charCode = this.currentCharCode();
+            if (charCode == EQUALS || charCode == GREATER_THAN || charCode == SLASH)
+                break;
+            this.advance();
+        }
+        if (startOffset < this.offset)
+            return this.string.substring(this.offset, startOffset);
+        return null;
+    }
+
+    consumeTagIdent() {
+        var startOffset = this.offset;
+        while (this.hasMoreChars() && !this.isWhitespaceChar()) {
+            var charCode = this.currentCharCode();
+            if (charCode == GREATER_THAN || charCode == SLASH)
+                break;
+            this.advance();
+        }
+        if (startOffset < this.offset)
+            return this.string.substring(this.offset, startOffset);
+        return null;
+    }
+
+    consumeStringUntil(stopChar) {
+        var startOffset = this.offset;
+        while (this.hasMoreChars()) {
+            var charCode = this.currentCharCode();
+            if (charCode == stopChar)
+                break;
+            this.advance();
+        }
+        if (startOffset < this.offset)
+            return this.string.substring(this.offset, startOffset);
+        return null;
+    }
+
+    skipWhitespace() {
+        while (this.hasMoreChars() && this.isWhitespaceChar())
+            this.advance();
+    }
+
+    isWhitespaceChar() {
+        var charCode = this.currentCharCode();
+        return charCode == SPACE ||
+            charCode == NEW_LINE ||
+            charCode == CARRIAGE_RETURN ||
+            charCode == TAB;
     }
 
     currentCharCode() {
@@ -47,6 +182,18 @@ class DomParser {
     hasMoreChars() {
         return this.offset < this.string.length;
     }
+
+    advance() {
+        this.offset++;
+    }
+
+    currentNode() {
+        return this.stack[this.stack.length - 1];
+    }
 }
 
 Object.preventExtensions(DomParser.prototype);
+
+exports.DomParser = DomParser;
+
+})(this);
--- dom/domparser_test.js
+++ dom/domparser_test.js
@@ -4,12 +4,48 @@
 "use strict";
 
 describe("DomParser", function() {
-    it("should parse a string into a dom tree", function() {
+    it("should parse a string into a Text", function() {
         var parser = new DomParser("Hello World");
         var fragment = parser.parse();
         var children = fragment.getChildNodes();
         assert.equal(children.length, 1);
         assert.instanceOf(children[0], Text);
         assert.equal(children[0].textContent, "Hello World");
+    });
+    it("should parse a string into an Element", function() {
+        var parser = new DomParser("<div>Hello World</div>");
+        var fragment = parser.parse();
+        var children = fragment.getChildNodes();
+        assert.equal(children.length, 1);
+        assert.instanceOf(children[0], Element);
+        assert.instanceOf(children[0].firstChild, Text);
+        assert.equal(children[0].firstChild.textContent, "Hello World");
+    });
+    it("should parse attributes on an element", function() {
+        var parser = new DomParser("<div id=10 other-bool other='foo' bar=\"baz bizzle\" bool></div>");
+        var fragment = parser.parse();
+        var children = fragment.getChildNodes();
+        assert.equal(children.length, 1);
+        var child = children[0];
+        assert.instanceOf(child, Element);
+        assert.isNull(child.firstChild);
+        assert.equal(child.getAttribute("id"), "10");
+        assert.equal(child.getAttribute("other"), "foo");
+        assert.equal(child.getAttribute("bar"), "baz bizzle");
+        assert.equal(child.getAttribute("bool"), "");
+        assert.equal(child.getAttribute("other-bool"), "");
+    });
+    it("should parse children of an element", function() {
+        var markup = "<div><span>A</span><span id=\"test\">B</span></div><span>C</span>";
+        var parser = new DomParser(markup);
+        var fragment = parser.parse();
+        var serializer = new DomSerializer();
+        assert.equal(serializer.serialize(fragment), markup);
+    });
+    it("should parse self closing tags", function() {
+        var parser = new DomParser("<span/>A<span id=10/ value=20>B<div />C");
+        var fragment = parser.parse();
+        var serializer = new DomSerializer();
+        assert.equal(serializer.serialize(fragment), "<span></span>A<span id=\"10\" value=\"20\">B<div></div>C</span>");
     });
 });
